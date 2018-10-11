a8faab5cc337c46bf5f457e655c89f4dd6b770b6
Felipe <felipe.ferreira0192@gmail.com>
2018-10-11 10:21:09 -0300

    Livelock

---
+++ src/livelock/Livelock.java
@@ -0,0 +1,25 @@
+package livelock;
+
+public class Livelock {
+    
+    static final Police police = new Police();
+ 
+    static final Criminal criminal = new Criminal();
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new Runnable() {
+            public void run() {
+                police.giveRansom(criminal);
+            }
+        });
+        t1.start();
+ 
+        Thread t2 = new Thread(new Runnable() {
+            public void run() {
+                criminal.releaseHostage(police);
+            }
+        });
+        t2.start();
+    }
+    
+}
